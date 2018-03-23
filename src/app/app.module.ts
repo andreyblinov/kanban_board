6ebbb241515357d552c1ab9e0a2ad7ecde5a63e6
Andrey <yuzhny91@gmail.com>
2018-03-23 19:00:32 +0200

    init project models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { KanbanListComponent } from './kanban-list/kanban-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KanbanListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/models/taskGroup.ts
@@ -0,0 +1,4 @@
+export interface TaskGroup {
+  id: number;
+  name: string;
+}
